71852e19ea560c55cfa53c08b570b4ffc5c599dd
livelykitten <ichupu9999@gmail.com>
2026-09-10 15:45:24 +0900

    First commit, with AppController created

---
+++ src/main/java/com/example/devops_service/AppController.java
@@ -0,0 +1,15 @@
+package com.example.devops_service;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController 
+public class AppController {
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Hello, World";
+    }
+    
+}
